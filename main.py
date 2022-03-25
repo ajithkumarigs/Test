c90ddbd706887c9f9771a02b302873145b472065
Ajithkumarigs <ajithkumara.igs@gmail.com>
2022-03-26 00:48:49 +0530

    ak good

---
+++ main.py
@@ -0,0 +1,10 @@
+a=10.2
+print (a)
+print(type(a))
+
+#use int,float,str
+
+a = int(input("enter the value of A : "))
+b = int(input("enter the value of B : "))
+c = a+b
+print("total : " + str(c))y
